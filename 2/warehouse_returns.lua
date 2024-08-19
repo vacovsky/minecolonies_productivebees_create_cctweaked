8cbe50ee1c4ace70a854a43d02a1013769db9886
Joe Vacovsky <vacovsky@gmail.com>
2024-08-19 11:44:32 -0700

    added warehouse kiosk

--- 2/warehouse_returns.lua
+++ 2/warehouse_returns.lua
@@ -1,4 +1,5 @@
-local DROPBOX = 'minecraft:barrel_0'
+local DROPBOX = 'minecraft:barrel_3'
+local WAREHOUSE = 'minecolonies:warehouse'
 
 function ReturnWares()
     dropbox = peripheral.wrap(DROPBOX)
@@ -6,16 +7,17 @@ function ReturnWares()
     peripherals = peripheral.getNames()
     for pni, perName in pairs(peripherals) do
         if string.find(perName, WAREHOUSE) then
-            print('Checking location:', perName)
             warehouses[#warehouses+1] = perName
         end
     end
 
-    for slot, item in pairs(dropbox) do
-        for whi, warehouse in warehouses(pairs) do 
-            dropbox.pushItems(peripheral.getName(warehouse), slot)
+    for slot, item in pairs(dropbox.list()) do
+        count = 0
+        for whi, warehouse in pairs(warehouses) do
+            print(warehouse,item.name, slot)
+            count = dropbox.pushItems(warehouse, slot)
         end
-        print('Returned', item.name)
+        print('Returned', item.name, count)
     end
 
     return true
