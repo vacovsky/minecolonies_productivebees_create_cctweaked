7034310dae42f451f6dd76df09db579240bccabc
Joe Vacovsky <vacovsky@gmail.com>
2024-08-20 09:22:03 -0700

    stubbed incinerator script

---
+++ 2/startup/incinerator.lua
@@ -0,0 +1,20 @@
+
+JUNKLIST_CHEST = ''
+
+TRASHAN = ''
+
+function IncinerateJunk()
+    -- JUNK ITEMS are defined by what's in the designated trash chest.
+
+    
+    -- LOAD JUNK ITEM LIST
+    junk = {}
+    
+    -- LOOP THROUGH JUNK CHEST TO CONFIRM IF ALL SHOULD BE DELETED
+
+    -- TRANSFER ITEMS OF EACH TYPE FROM WAREHOUSE TO INCINERATOR WITH .5 SECONDS DELAY
+end
+
+
+print('Starting junk incinerator...')
+pcall(IncinerateJunk)
