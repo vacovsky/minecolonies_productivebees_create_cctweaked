ef428cf65e31b8c437c34a912d43b027d2144f15
Joe Vacovsky <vacovsky@gmail.com>
2024-08-20 16:40:04 -0700

    multi warehouse support

--- 1/honey_bottler.lua
+++ 1/honey_bottler.lua
@@ -1,15 +1,24 @@
 local WAIT_SECONDS = 30
-local warehouse = peripheral.find("minecolonies:warehouse")
 local REBOOT_AFTER_LOOPS = 60 -- REBOOT AFTER THIS MANY LOOPS
 local warehouses = "minecolonies:warehouse"
+local honey_storage = 'fluidTank_0'
+local honey_bottler = 'create:depot_0'
 
+
+-- function FindAllWarehouses()
+--     warehouses = {}
+--     peripherals = peripheral.getNames()
+--     for pni, perName in pairs(peripherals) do
+--         if string.find(perName, WAREHOUSE) then
+--             warehouses[#warehouses+1] = perName
+--         end
+--     end
+--     return warehouses
+-- end
 
 function Main()
-
     peripherals = peripheral.getNames()
-    honey_storage = 'fluidTank_0'
-    honey_bottler = 'create:depot_0'
-
+    print('\n')
     -- HONEY BOTTLER
     for index, attached_peripheral in pairs(peripherals) do
         if string.find(attached_peripheral, honey_bottler) then
@@ -17,49 +26,67 @@ function Main()
             -- PLACE FILLED BOTTLES IN WAREHOUSE
             for slot, item in pairs(container.list()) do
                 if item.name == 'minecraft:honey_bottle' then
-                    print('Warehousing honey bottles')
-                    DepositInAnyWarehouse(container, slot)
+                    local depositNum = DepositInAnyWarehouse(container, slot)
+                    if depositNum > 0 then print('Warehoused', depositNum, 'honey bottles') end
                 end
             end
             -- REFILL WITH EMPTY BOTTLES
-            GetFromAnyWarehouse('minecraft:glass_bottle', container)
-            
+            local restockNum = GetFromAnyWarehouse('minecraft:glass_bottle', peripheral.getName(container), 64, false)
+            if restockNum > 0 then print('Restocked', restockNum, 'glass bottles') end
         end
     end
 end
 
 function DepositInAnyWarehouse(sourceStorage, sourceSlot)
-    peripherals = peripheral.getNames()
-    warehouses_list = {}
+    local movedItemCount = 0
+    local peripherals = peripheral.getNames()
+    local warehouses_list = {}
     for index, attached_peripheral in pairs(peripherals) do
         if string.find(attached_peripheral, warehouses) then
             warehouses_list[#warehouses_list+1] = attached_peripheral
         end
     end
     for whi, warehouse in pairs(warehouses_list) do
-        TransferItem(sourceStorage, sourceSlot, warehouse)
+        movedItemCount = movedItemCount + sourceStorage.pushItems(warehouse, sourceSlot)
     end
+    return movedItemCount
 end
 
-function GetFromAnyWarehouse(itemName, destination)
-    peripherals = peripheral.getNames()
-    warehouses_list = {}
+function GetFromAnyWarehouse(itemName, destination, itemCount, guess)
+    -- COLLECT WAREHOUSE NAMES
+    local peripherals = peripheral.getNames()
+    local warehouses_list = {}
     for index, attached_peripheral in pairs(peripherals) do
         if string.find(attached_peripheral, warehouses) then
+            -- print(attached_peripheral)
             warehouses_list[#warehouses_list+1] = attached_peripheral
         end
     end
+
+    -- SEARCH EACH WAREHOUSE FOR ITEM
+    local foundCount = 0
     for whi, warehouse in pairs(warehouses_list) do
-        warehouse_peripheral = peripheral.wrap(warehouse)
-        for slot, item in pairs(warehouse_peripheral.list()) do
-            if item.name == itemName then
-                print('Restocking', itemName)
-                TransferItem(warehouse_peripheral, slot, destination)
-                goto found
+        local whp = peripheral.wrap(warehouse)
+        for slot, item in pairs(whp.list()) do
+            -- must be exact name match
+            if not guess then
+                if item.name == itemName then
+                    local pushedCount = whp.pushItems(destination, slot, itemCount - foundCount)
+                    foundCount = foundCount + pushedCount
+                    if foundCount >= itemCount then print('Order successfully filled!')
+                        -- EXIT WHEN WE HAVE DELIVERED ENOUGH
+                        print('Returned', itemCount, itemName)
+                        goto found
+                    end
+                end
             end
+            -- TODO fuzzy match here
+            -- end fuzzy
         end
+        if itemCount < foundCount then print('Only located', foundCount, 'of', itemCount) end
         ::found::
     end
+    return foundCount
 end
 
 
@@ -75,7 +102,8 @@ local LOOPS = 0
 print('Starting HONEY BOTTLER')
 while true do
     if redstone.getInput('top') then
-        pcall(Main)
+        -- pcall(Main)
+        Main()
     else
         print('Service Offline - Flip the lever on top!')
     end
