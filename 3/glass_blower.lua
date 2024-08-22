77043c9992106616389484e6f6a8e694cd3754eb
Joe Vacovsky <vacovsky@gmail.com>
2024-08-21 19:30:01 -0700

    added bottle maker turtle

--- 3/glass_blower.lua
+++ 3/glass_blower.lua
@@ -1,5 +1,5 @@
 local whi = require 'warehouse_interface'
 
-whi.GetFromAnyWarehouse('minecraft:coal', 'minecraft:furnace_5', 64, 1, false)
-whi.GetFromAnyWarehouse('minecraft:sand', 'minecraft:furnace_5', 64, 2, false)
-whi.DepositInAnyWarehouse(peripheral.wrap('furnace_5'), 3)
+-- whi.GetFromAnyWarehouse('minecraft:coal', 'minecraft:furnace_5', 64, 1, false)
+-- whi.GetFromAnyWarehouse('minecraft:sand', 'minecraft:furnace_5', 64, 2, false)
+-- whi.DepositInAnyWarehouse(peripheral.wrap('furnace_5'), 3)
