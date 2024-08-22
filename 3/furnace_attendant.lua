5a3bc6723132a7cd7c3dae2da9dd3c1a81347e88
Joe Vacovsky <vacovsky@gmail.com>
2024-08-21 19:56:54 -0700

    furnace_attendant

--- 3/furnace_attendant.lua
+++ 3/furnace_attendant.lua
@@ -5,8 +5,8 @@ local fuel = 'minecraft:coal'
 local raw_item = 'minecraft:sand'
 
 while true do
-    print(whi.GetFromAnyWarehouse(false, fuel,furnace, 16, 1))
-    print(whi.GetFromAnyWarehouse(false, raw_item, furnace, 16, 2))
+    print(whi.GetFromAnyWarehouse(false, raw_item, furnace, 64, 1))
+    print(whi.GetFromAnyWarehouse(false, fuel, furnace, 64, 2))
     print(whi.DepositInAnyWarehouse(furnace, 3))
-    sleep(300)
+    sleep(120)
 end
