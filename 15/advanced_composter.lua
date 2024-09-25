d0938fe02eaea159ada0ab4765b94ef353e47a90
Joe Vacovsky <vacovsky@gmail.com>
2024-09-24 17:26:45 -0700

    aliens hate fire

--- 15/advanced_composter.lua
+++ 15/advanced_composter.lua
@@ -8,15 +8,20 @@ local avanced_composter_output = 'minecraft:hopper_7'
 local input_item = 'minecraft:wheat'
 
 function LoadComposter()
+    -- add compostable items
     local input_count = 0
-    local output_count = 0
     input_count = input_count + whi.GetFromAnyWarehouse(true, input_item, avanced_composter_input, 64)
-    if input_count > 0 then print(count, 'inputs') end
-    output_count = output_count + whi.DepositInAnyWarehouse(avanced_composter_output)
-    if output_count > 0 then print(count, 'outputs') end
+    if input_count > 0 then print(input_count, 'inputs') end
+
+    -- collect outputs
+    local output_count = 0
+    for slot, _ in pairs(peripheral.wrap(avanced_composter_output).list()) do
+        output_count = output_count + whi.DepositInAnyWarehouse(avanced_composter_output, slot)
+    end
+    if output_count > 0 then print(output_count, 'outputs') end
 end
 
 while true do
     LoadComposter()
-    sleep(20)
+    sleep(15)
 end
